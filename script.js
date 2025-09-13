586157e67022497a8b9eb233349d9a1c10cd2b0a
MazeQhi404 <maze.putuma@gmail.com>
2025-09-13 18:47:31 +0200

    imported arrays from data.js file, created a class with a constructor that uses 'this' instances and defines the function init

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /** PodcastApp class to manage the app */
-
+import { podcasts, genres, seasons } from './data.js';
 class PodcastApp {
     constructor(podcasts, genres, seasons) {
         this.podcasts = podcasts;
@@ -8,6 +8,19 @@ class PodcastApp {
         this.podcastList = document.querySelector(".podcast-list");
         this.modal = document.querySelector("#podcast-modal");
         this.init ();
+
     }
+
+    /**
+     * Initialises the app by rendering podcasts and setting up event listeners.
+     */
+    init() {
+        this.renderPodcasts();
+        this.setupEventListeners()
+    }
+   
 }
+
+
+/* General Notes */ 
 
