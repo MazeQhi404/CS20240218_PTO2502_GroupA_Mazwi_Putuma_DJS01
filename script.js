b5499958eb1a5e90505806f0880ec406082fd8d2
MazeQhi404 <maze.putuma@gmail.com>
2025-09-14 21:41:52 +0200

    created a method that creates an article element representing a podcast card; populated with the information stored in the podcast objects and attached event listeners to show their modals

--- script.js
+++ script.js
@@ -28,6 +28,7 @@ class PodcastApp {
     return genreIds.map(id => {
         const genre = this.genres.find(g => g.id === id);
         return genre ? genre.title : "Unknown";
+        //TEST: change id of a podcast in podcasts.id to number that is not available in genres array.
     });
    }
 
@@ -39,6 +40,7 @@ class PodcastApp {
     this.podcastList.innerHTML = ""; //Clears existing content
     this.podcasts.forEach(podcast => {
         const card = this.createPodcastCard(podcast);
+        //TEST: console.log(card)
         this.podcastList.appendChild(card);
     });
    }
@@ -50,7 +52,29 @@ class PodcastApp {
     */
    createPodcastCard(podcast) {
     const card = document.createElement("article");
-    card.
+    card.classList.add("podcast-card");
+    //card.dataset.id = podcast.id; // dataset provides access to all custom data attibutes on an HTML element (i.e. <article class="podcast-card" data-id="10716">)
+    card.tabIndex = 0; // Make card focusable for accessibility
+    const genreTitles = this.getGenreTitles(podcast.genres);
+    card.innerHTML = `
+        <img src="${podcast.image}" alt="${podcast.title} cover" class="podcast-image" >
+        <h2 class=podcast-title">${podcast.title}</h2>
+        <p class="podcast-seasons">Seasons: ${podcast.seasons}</p>
+        <p class="podcast-genres">Genres: ${genreTitles.join(", ")}</p>
+        <p class="podcast-updated">Last Updated: ${new Date(podcast.updated).toLocaleDateString("en-GB", {
+            year: "numeric",
+            month: "long",
+            day: "numeric"
+        })}</p>
+    `;
+    card.addEventListener("click", () => this.showModal(podcast));
+    card.addEventListener("keypress", (e) => {
+        if (e.key === "Enter" || e.key === " ") { // e is a parameter representing the event object (contains info about the event that triggered the listener, i.e a keypress)
+            this.showModal(podcast);
+            //TEST: console.log(e.key);
+        }
+    });
+    return card;
    }
 
 
